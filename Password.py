ab4f89812e5d28814fd6ecd6bf9d21161e7f56d8
ptilanom <pablo.tilano@sanjosevegas.edu.co>
2024-06-05 11:36:03 -0500

    Finalizado Revisado

--- Password.py
+++ Password.py
@@ -1,34 +1,35 @@
-# Solicitar la contraseña al usuario
-contrasena = input("Ingresa tu contraseña: ")
-
 def verificar_longitud(contrasena):
     # Verifica la longitud de la contraseña
     if len(contrasena) < 8:
-        print("Tu contraseña debe tener al menos 8 caracteres")
+        print("- Tu contraseña debe tener al menos 8 caracteres")
         return False
     return True
 
 def verificar_mayusculas(contrasena):
     # Verifica si hay al menos una mayúscula
     if not any(c.isupper() for c in contrasena):
-        print("Tu contraseña debe contener al menos una letra mayúscula")
+        print("- Tu contraseña debe contener al menos una letra mayúscula")
         return False
     return True
 
 def verificar_numero(contrasena):
     # Verifica si hay al menos un número
     if not any(c.isdigit() for c in contrasena):
-        print("Tu contraseña debe contener al menos un número")
+        print("- Tu contraseña debe contener al menos un número")
         return False
     return True
 
-# Verificar cada condición
-longitud_valida = verificar_longitud(contrasena)
-mayuscula_valida = verificar_mayusculas(contrasena)
-numero_valido = verificar_numero(contrasena)
+def es_contrasena_segura(contrasena):
+    longitud_valida = verificar_longitud(contrasena)
+    mayuscula_valida = verificar_mayusculas(contrasena)
+    numero_valido = verificar_numero(contrasena)
+    return longitud_valida and mayuscula_valida and numero_valido
 
-# Si todas las condiciones se cumplen, la contraseña es segura
-if longitud_valida and mayuscula_valida and numero_valido:
-    print("Tu contraseña es segura")
-else:
-    print("Tu contraseña no es segura")
+# Solicitamos la contraeña y utilizamos la funcion que contiene las demas para verificar la contraseña y permitimos otro intento
+while True:
+    contrasena = input("Ingresa tu contraseña: ")
+    if es_contrasena_segura(contrasena):
+        print("Tu contraseña es segura")
+        break
+    else:
+        print("Intenta nuevamente.")
